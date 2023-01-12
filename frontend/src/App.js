d46163d148d034481736d2e6f8df766eed1a3f20
Yarince90 <Yarince.vigil@gmail.com>
2023-01-11 22:05:07 -0500

    Original Commit

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          What Note To-Do App
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
