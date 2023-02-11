415a373fd3b7943afc4de373683db2e3b8a842ae
Yarince90 <Yarincevigil@gmail.com>
2023-02-10 22:04:47 -0500

    Moved createNote area to app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,10 +2,18 @@ import React, { useState } from "react";
 import './App.css';
 import Header from './components/header/Header';
 import Note from "./components/note/Note";
-import CreateNote from "./components/createNote/CreateNote";
+import Fab from '@mui/material/Fab';
+import Zoom from '@mui/material/Zoom';
+import AddCircleRoundedIcon from '@mui/icons-material/AddCircleRounded';
 
 function App() {
   const [notes, setNotes] = useState([]);
+  const [isExpanded, setExpanded] = useState(false);
+  const [note, setNote] = useState({
+    title: "",
+    content: ""
+});
+let isFocused = false;
 
   function addNote(newNote) {
     setNotes(prevNotes => {
@@ -21,34 +29,78 @@ function App() {
     });
   }
 
-  //------------------------------Remove focus -------------------------------------
+  function handleChange(e){
+    const{ name, value } = e.target;
+
+    setNote(prevNote => {
+        return {
+            ...prevNote,
+            [name]: value
+        };
+    });
+}
+
+function submitNote(event) {
+  addNote(note);
+  setNote({
+    title: "",
+    content: ""
+  });
+  event.preventDefault();
+}
+
+  //------------------------------Handle focus -------------------------------------
   document.body.addEventListener('click', function(e){
-
-    //function removeFocus(){  
       if (e.target.classList.contains('createNote')){
-        //contract(); ---Pass to CreateNote to contract
+        isFocused = true;
         console.log("createNote Area Clicked!!!");
+        setExpanded(true);
       }
 
-      else {
+      else if (!e.target.classList.contains('createNote')){
+        isFocused = false;
         console.log('Not Clicked');
+        setExpanded(false);
       }
-
-      console.log()
-      
-
-    //}
-    
   })
+
+  function handleFocus(){
+    if(isFocused){
+      setExpanded(true);
+    } else {
+      setExpanded(false);
+    }
+  }
 
   
 
   return (
     <div className="App">
       <Header />
-      <CreateNote
-        onAdd={addNote}
-      />
+      <form className="createNote">
+        {isExpanded && (
+          <input className="createNote"
+            name="title"
+            onChange={handleChange}
+            value={note.title}
+            placeholder="Title"
+          />
+          )}
+          <textarea className="createNote"
+            name="content"
+            onClick={handleFocus}
+            onChange={handleChange}
+            value={note.content}
+            placeholder="Add note ..."
+            rows={isExpanded ? 3 : 1}
+            />
+            <Zoom in={isExpanded}>
+              <Fab onClick={submitNote}>
+                <AddCircleRoundedIcon />
+              </Fab>
+            </Zoom>
+        </form>
+    
       {notes.map((noteItem, index) => {
         return(
           <Note
