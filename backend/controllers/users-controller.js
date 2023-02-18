0a42a033f0bedf02766aa52dab513cc8d79d03fd
Yarince90 <Yarincevigil@gmail.com>
2023-02-17 23:26:37 -0500

    Added authentication logic to registration user controller

--- backend/controllers/users-controller.js
+++ backend/controllers/users-controller.js
@@ -1,26 +1,63 @@
+const { validationResult } = require('express-validator');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const User = require('../models/user');
 const HttpError = require('../models/http-errors');
 
-
+//get All users
 const getUser = (req, res, next) => {
 console.log('Connected to users!')
 }
 
+//Log user into account
 const logIn = (req, res, next) => {
 
 }
 
+//Create user account
 const register = async (req, res, next) => {
-    const { userName, password } = req.body;
+    const { userName, email, password } = req.body;
+    const errors = validationResult(req);
 
+    //Check for errors
+    if (!errors.isEmpty()) {
+        return next(
+          new HttpError('Invalid data was entered.', 422)
+        );
+        }
+    
+
+    //Check for existing user by email
+    let existingUser;
+    try {
+        existingUser = await User.findOne({ email });
+    } catch(err) {
+        const error = new HttpError(
+            'User already exists.', 422
+          );
+          return next(error);
+    }
+
+    //Hash Password
+    let hashedPassword;
+    try {
+        hashedPassword = await bcrypt.hash(password, 12);
+    } catch(err) {
+        const error = new HttpError(
+            'Error creating user. Please try again', 500
+          );
+          return next(error);
+    }
+
+
+    //Create new user
     const createUser = new User ({
+        email,
         userName,
-        password
+        password: hashedPassword
     });
 
-   try{
+   try {
     await createUser.save()
    }catch (err){
     const error = new HttpError(
@@ -29,11 +66,25 @@ const register = async (req, res, next) => {
     return next(error);
    }
 
+   //Create login token 
+   let token;
+   try {
+    token = jwt.sign(
+        {   userId: createUser.id, email: createUser.email  },
+        'SuperSecretCode_DO_NOT_SHARE',
+        {   expiresIn: '1h' }
+    )
+   } catch (err) {
+    const error = new HttpError(
+      'Signing up failed, please try again later.',
+      500
+    );
+    return next(error);
+  }
+
+    //Server response
     res.status(201)
-    .json({
-        userName: createUser.userName,
-        password: createUser.password
-    })
+    .json({userId: createUser.id, email: createUser.email, token: token})
 }
 
 
