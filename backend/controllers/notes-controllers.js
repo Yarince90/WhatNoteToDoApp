64aaf2180dc943db1eb1c3298ebb47008b957a7a
Yarince90 <Yarincevigil@gmail.com>
2023-02-21 20:44:14 -0500

    Created Notes DB Model and added creates and get notes controller

--- backend/controllers/notes-controllers.js
+++ backend/controllers/notes-controllers.js
@@ -1,24 +1,67 @@
-const getNotes = (req, res, next) => {
-    console.log('Connected to Notes!')
+const { validationResult } = require('express-validator');
+const Note = require('../models/note');
+const User = require('../models/user')
+const HttpError = require('../models/http-errors');
+
+//Get all notes
+const getNotes = async (req, res, next) => {
+    let notes;
+    try {
+        notes = await Note.find();
+    } catch (err) {
+        const error = new HttpError(
+            'Failed to get all notes.', 500
+          );
+          return next(error);
+    }
+    res.json({notes: notes.map(note => note.toObject({getters: true}))});
 }
 
-const getNotesById = (req, res, next) => {
-
-}
-
-const createNote = (req, res, next) => {
-    const {title, content, creator} = req.body;
+//Create new note
+const createNote = async (req, res, next) => {
+    const {title, content } = req.body;
 
     const createdNote = {
         title,
         content,
-        creator
+        creator: req.userData.userId
     }
 
-    newNote.push(createdNote);
+    //Look for current active user
+    let user;
+    try {
+        user = await User.findById(req.userData.userId);
+    } catch (err) {
+        const error = new HttpError(
+            'Unable to create note', 500
+          );
+          return next(error);
+    }
+
+    if (!user) {
+        const error = new HttpError('Could not create note --Unable to locate user.', 404);
+        return next(error);
+      }
+    
+      console.log(user);
+
+      try {
+        const session = await mongoose.startSession();
+        session.startSession();
+
+        await createdNote.save({session: session});
+        user.notes.push(createdNote);
+        await user.save({ session: session });
+        await session.commitTransaction();
+      } catch (err) {
+        const error = new HttpError(
+            'Unable to create note.', 500
+          );
+          return next(error);
+      }
+
     res.status(201).json(createdNote);
 }
 
 exports.getNotes = getNotes;
-exports.getNotesById = getNotesById;
 exports.createNote = createNote;
