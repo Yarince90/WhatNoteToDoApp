d574d0e5fabf17cc5e89b583ff3822286f0fb78a
Yarince90 <Yarincevigil@gmail.com>
2023-02-25 12:08:16 -0500

    Fixed Http Hook - sendRequest function

--- frontend/src/hooks/http-hook.js
+++ frontend/src/hooks/http-hook.js
@@ -1,56 +1,22 @@
-import { useState, useCallback, useRef, useEffect } from 'react';
+import { useCallback } from 'react';
 
 export const useHttpClient = () => {
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState();
-
-  const activeHttpRequests = useRef([]);
-
+  
   const sendRequest = useCallback(
     async (url, method = 'GET', body = null, headers = {}) => {
-      setIsLoading(true);
-      const httpAbortCtrl = new AbortController();
-      activeHttpRequests.current.push(httpAbortCtrl);
+        try {
+            const response = await fetch(url, {
+                method,
+                body,
+                headers
+            });
+            const resData = await response.json();
+            
+            return resData;
 
-      try {
-        const response = await fetch(url, {
-          method,
-          body,
-          headers,
-          signal: httpAbortCtrl.signal
-        });
+        } catch (err){console.log(err)}
 
-        const responseData = await response.json();
+    },[]);
 
-        activeHttpRequests.current = activeHttpRequests.current.filter(
-          reqCtrl => reqCtrl !== httpAbortCtrl
-        );
-
-        if (!response.ok) {
-          throw new Error(responseData.message);
-        }
-
-        setIsLoading(false);
-        return responseData;
-      } catch (err) {
-        setError(err.message);
-        setIsLoading(false);
-        throw err;
-      }
-    },
-    []
-  );
-
-  const clearError = () => {
-    setError(null);
-  };
-
-  useEffect(() => {
-    return () => {
-      // eslint-disable-next-line react-hooks/exhaustive-deps
-      activeHttpRequests.current.forEach(abortCtrl => abortCtrl.abort());
-    };
-  }, []);
-
-  return { isLoading, error, sendRequest, clearError };
+    return { sendRequest };
 };
